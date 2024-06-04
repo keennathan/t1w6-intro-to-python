5d5fd466cc43baa5d12b00023a3edaa8d47cd366
keennathan <keennathan@icloud.com>
2024-06-04 20:46:20 +1000

    made variables

---
+++ another_main.py
@@ -0,0 +1,25 @@
+# variables
+
+num1 = 5
+num2 = 7
+
+print(num1 + num2)
+
+num2 = 1
+
+print(num1 + num2)
+
+# assign the value of num2 to num1
+num1 = num2 
+print(num1 + num2)
+
+num3 = 5
+num3 = num3+2
+print(num3)
+
+num3+= 3
+print(num3)
+total = num1+num2+num3
+# string iterpolation
+print("the total addition is",+total)
+print(f"the total addition is {total}")
--- main.py
+++ main.py
@@ -1,2 +1,72 @@
-print("Hello World!")
-print(5/4)
+# print("Hello World!")
+# print(5/4)
+# print("don't forget to commit")
+
+# print("it's a comment, it won't be printed")
+
+# data types
+
+# integers 
+
+# 4
+# 7
+# 9
+# print(9)
+# print("9")
+# print(type("9"))
+# print(type (9))
+
+# # Floats
+
+# 2.5
+# 5.123123
+# print(3.1415)
+# print(type(-5.63))
+# print(type(-5))
+
+# # complex (imaginary numbers),
+ 
+# 2+3j
+# print(type(4+7j))
+
+# operators + - * / %
+
+# print(4 * 5)
+# print(15 / 3)
+# print(15 / 2)
+# print(7 % 2)
+
+# string
+
+# "Hello World!"
+# 'Hello World!'
+# print("This is a String")
+
+# # string concatenations, addition, join
+
+# print("hello" + "world")
+# print(3+5)
+# print("3"+"5")
+
+# boolean
+# True
+# False
+# print(type(False))
+# 0 # falsy value
+# "" # falsy value
+# print(bool(0))
+# print(bool(""))
+# print(bool(-123))
+# print(bool("False"))
+
+# Comparison operators <, >, <=, >=, ==, !=
+# print(2 < 3)
+# print(5 >= 5)
+# print("hello" == "hello")
+# print("HELLO".lower())
+# print("HELLO".lower()=="hello")
+
+# logical operators and, or, not
+print(True and False) # AND returns true when both operators are true
+print(False or True) # OR returns true, when one or both of the operators are true
+print(not False) # NOT returns the opersite 
