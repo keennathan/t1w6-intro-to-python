03793c942b34d11f69aaa8e7ec057895675dddee
keennathan <keennathan@icloud.com>
2024-06-04 21:02:38 +1000

    programs with inputs

---
+++ area_of_square.py
@@ -0,0 +1,8 @@
+#get the length from the user
+length = int(input("Enter the length of the square:"))
+# print(length)
+
+#calculate and store the area in a variable
+area = length * length
+#print the calculated area to the terminal
+print(f"The calculated area is {area}")
---
+++ create_email_form.py
@@ -0,0 +1,4 @@
+first_name = input("Please enter your first name(no blank spaces)")
+last_name = input("Please enter your last name(no blank spaces)")
+company_name = input("Please enter your company name(no blank spaces)")
+print(f"{first_name}.{last_name}@{company_name}.com".lower())
